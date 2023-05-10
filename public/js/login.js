b79f0d5613a1e565c85e109142acc05f9ee44031
EddieQVQ <122253032+EddieQVQ@users.noreply.github.com>
2023-05-10 15:32:02 -0700

    Eddie features page (#12)
    * commit
    
    * Delete node_modules directory
    
    * Delete .env
    
    * Delete package-lock.json
    
    ---------
    
    Co-authored-by: mrnoah99 <83614847+mrnoah99@users.noreply.github.com>

--- public/js/login.js
+++ public/js/login.js
@@ -1,61 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const loginButton = document.getElementById("login-button");
 
-const path = require('path');
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const UserSchema = new Schema({
-  username: String,
-  password: String,
-  // include other properties as required
+loginButton.addEventListener("click", () => {
+  window.location.href = "index.html";
 });
 
-const User = mongoose.model('User', UserSchema);
+const signupButton = document.getElementById("gotosignup-button");
 
-
-mongoose.connect('mongodb://localhost/COMP2800-BBY-12', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const session = require('express-session');
-
-app.use(session({
-  secret: 'your secret key',
-  resave: false,
-  saveUninitialized: true
-}));
-
-app.get('/login', (req, res) => {
-  res.render('login');
+signupButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  window.location.href = "signup.html";
 });
-
-app.post('/login', async (req, res) => {
-  const user = await User.findOne({ username: req.body.username, password: req.body.password });
-  if (user) {
-    req.session.user = user;
-    res.redirect('/');
-  } else {
-    res.redirect('/login');
-  }
-});
-
-app.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.redirect('/login');
-});
-
-app.get('/', (req, res) => {
-  if (req.session.user) {
-    res.send('Welcome, ' + req.session.user.username);
-  } else {
-    res.redirect('/login');
-  }
-});
-
-app.listen(3000, () => console.log('Server is running on port 3000'));
--- public/js/signup 2.js
+++
@@ -1,7 +0,0 @@
-const phoneInput = document.getElementById('phone');
-
-phoneInput.addEventListener('input', () => {
-  const input = phoneInput.value.replace(/\D/g, '').substring(0,10);
-  const formatted = input.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3'); 
-  phoneInput.value = formatted;
-});
