2d3dc4cd4a0fd3254b9b62a2d22e11d96ef2e63a
Sohee Hwang <122145055+SOHEE990302@users.noreply.github.com>
2023-05-11 15:22:14 -0700

    Sohee add profile page function (#20)
    * free game html,css,ejs
    
    * free and popular game css
    
    * free, popular html and css
    
    * free and popular ejs
    
    * .gitignore
    
    * link to filter
    
    * profile.js
    
    * not complete recommand page
    
    * popular page add show detal button
    
    * free gamd add show detail button
    
    * popular game.ejs
    
    * popular.ejs
    
    * profile page
    
    * profile.ejs
    
    * recommand page html
    
    * recommand html, css, ejs, javascript
    
    * profile and recommend ejs connect complete
    
    * end and gitignore
    
    * popular.ejs connect
    
    * free game ejs completely connect
    
    * game detail html, ejs, index.js
    
    * env and index.js
    
    * free, popular, recommend html, css, ejs
    
    * connect free ejs
    
    * add game detail page
    
    * add header and footer tag on ejs file
    
    * profile page change password function of javascript
    
    * profile page function add

--- public/js/profile.js
+++ public/js/profile.js
@@ -46,3 +46,67 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   }
   
+
+  document.getElementById('image-upload').addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    if (file) {
+        // Update the image preview
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('profile-img').src = e.target.result;
+        }
+        reader.readAsDataURL(file);
+        
+        // Send the image to the server
+        const formData = new FormData();
+        formData.append('image', file);
+        fetch('/upload-profile-image', { // Replace this with the correct API endpoint
+            method: 'POST',
+            body: formData
+        }).then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert('Profile image updated successfully.');
+                // The server should return the URL of the new image
+                document.getElementById('profile-img').src = data.imageUrl;
+            } else {
+                alert('Error updating profile image. Please try again.');
+            }
+        }).catch(error => {
+            console.error('Error:', error);
+            alert('Error updating profile image. Please try again.');
+        });
+    }
+});
+
+
+function saveProfile() {
+    // Get the input values
+    const nickname = document.getElementById('nickname').value;
+    const email = document.getElementById('email').value;
+
+    // Create an object to send to the server
+    const profileData = {
+        nickname: nickname,
+        email: email
+    };
+
+    // Send the data to the server
+    fetch('/update-profile', { // Replace this with the correct API endpoint
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(profileData)
+    }).then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            alert('Profile updated successfully.');
+        } else {
+            alert('Error updating profile. Please try again.');
+        }
+    }).catch(error => {
+        console.error('Error:', error);
+        alert('Error updating profile. Please try again.');
+    });
+}
