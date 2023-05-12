ebfa636252744259a8eb56bef3a4e3521d843e17
SeungyeopHyun <shyun9@my.bcit.ca>
2023-05-12 13:24:51 -0700

    modify forgotpw.ejs file

--- index.js
+++ index.js
@@ -37,139 +37,139 @@ const userCollection = database.db(mongodb_database).collection("users");
 app.use(express.urlencoded({ extended: false }));
 
 var mongoStore = MongoStore.create({
-	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}?retryWrites=true&w=majority`,
-	crypto: {
-		secret: mongodb_session_secret
-	}
+  mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}?retryWrites=true&w=majority`,
+  crypto: {
+    secret: mongodb_session_secret
+  }
 })
 
-app.use(session({ 
+app.use(session({
   secret: node_session_secret,
-	store: mongoStore, //default is memory store 
-	saveUninitialized: false, 
-	resave: true
+  store: mongoStore, //default is memory store 
+  saveUninitialized: false,
+  resave: true
 }
 ));
 
 
-  app.get('/nosql-injection', async (req,res) => {
-    var name = req.query.user;
-  
-    if (!name) {
-      res.send(`<h3>no user provided - try /nosql-injection?user=name</h3> <h3>or /nosql-injection?user[$ne]=name</h3>`);
-      return;
-    }
-    //console.log("user: "+name);
-  
-    const schema = Joi.string().max(100).required();
-    const validationResult = schema.validate(name);
-  
-      var invalid = false;
-    //If we didn't use Joi to validate and check for a valid URL parameter below
-    // we could run our userCollection.find and it would be possible to attack.
-    // A URL parameter of user[$ne]=name would get executed as a MongoDB command
-    // and may result in revealing information about all users or a successful
-    // login without knowing the correct password.
-    if (validationResult.error != null) { 
-          invalid = true;
-        console.log(validationResult.error);
+app.get('/nosql-injection', async (req, res) => {
+  var name = req.query.user;
+
+  if (!name) {
+    res.send(`<h3>no user provided - try /nosql-injection?user=name</h3> <h3>or /nosql-injection?user[$ne]=name</h3>`);
+    return;
+  }
+  //console.log("user: "+name);
+
+  const schema = Joi.string().max(100).required();
+  const validationResult = schema.validate(name);
+
+  var invalid = false;
+  //If we didn't use Joi to validate and check for a valid URL parameter below
+  // we could run our userCollection.find and it would be possible to attack.
+  // A URL parameter of user[$ne]=name would get executed as a MongoDB command
+  // and may result in revealing information about all users or a successful
+  // login without knowing the correct password.
+  if (validationResult.error != null) {
+    invalid = true;
+    console.log(validationResult.error);
     //    res.send("<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>");
     //    return;
-    }	
-      var numRows = -1;
-      //var numRows2 = -1;
-      try {
-        const result = await userCollection.find({name: name}).project({username: 1, password: 1, _id: 1}).toArray();
-        //const result2 = await userCollection.find("{name: "+name).project({username: 1, password: 1, _id: 1}).toArray(); //mongoDB already prevents using catenated strings like this
-          //console.log(result);
-          numRows = result.length;
-          //numRows2 = result2.length;
-      }
-      catch (err) {
-          console.log(err);
-          res.send(`<h1>Error querying db</h1>`);
-          return;
-      }
-  
-      console.log(`invalid: ${invalid} - numRows: ${numRows} - user: `,name);
-  
-      // var query = {
-      //     $where: "this.name === '" + req.body.username + "'"
-      // }
-  
-      // const result2 = await userCollection.find(query).toArray(); //$where queries are not allowed.
-      
-      // console.log(result2);
-  
-      res.send(`<h1>Hello</h1> <h3> num rows: ${numRows}</h3>`); 
-      //res.send(`<h1>Hello</h1>`);
+  }
+  var numRows = -1;
+  //var numRows2 = -1;
+  try {
+    const result = await userCollection.find({ name: name }).project({ username: 1, password: 1, _id: 1 }).toArray();
+    //const result2 = await userCollection.find("{name: "+name).project({username: 1, password: 1, _id: 1}).toArray(); //mongoDB already prevents using catenated strings like this
+    //console.log(result);
+    numRows = result.length;
+    //numRows2 = result2.length;
+  }
+  catch (err) {
+    console.log(err);
+    res.send(`<h1>Error querying db</h1>`);
+    return;
+  }
 
+  console.log(`invalid: ${invalid} - numRows: ${numRows} - user: `, name);
+
+  // var query = {
+  //     $where: "this.name === '" + req.body.username + "'"
+  // }
+
+  // const result2 = await userCollection.find(query).toArray(); //$where queries are not allowed.
+
+  // console.log(result2);
+
+  res.send(`<h1>Hello</h1> <h3> num rows: ${numRows}</h3>`);
+  //res.send(`<h1>Hello</h1>`);
+
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.post("/signupSubmit", async (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
+  var email = req.body.email;
+  var phone = req.body.phone;
+
+  const schema = Joi.object({
+    username: Joi.string().alphanum().max(20).required(),
+    password: Joi.string().max(20).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().pattern(/^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/).required(),
   });
 
-  app.get("/signup", (req, res) => {
-    res.render("signup");
-  });
-  
-  app.post("/signupSubmit", async (req, res) => {
-    var username = req.body.username;
-    var password = req.body.password;
-    var email = req.body.email;
-    var phone = req.body.phone;
-  
-    const schema = Joi.object({
-      username: Joi.string().alphanum().max(20).required(),
-      password: Joi.string().max(20).required(),
-      email: Joi.string().email().required(),
-      phone: Joi.string().pattern(/^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/).required(),
-    });
+  const validationResult = schema.validate({ username, email, password, phone });
+  if (validationResult.error != null) {
+    console.log(validationResult.error);
+    res.redirect("/signup");
+    return;
+  }
 
-    const validationResult = schema.validate({ username, email, password, phone });
-    if (validationResult.error != null) {
-      console.log(validationResult.error);
-      res.redirect("/signup");
-      return;
-    }
-  
-    var hashedPassword = await bcrypt.hash(password, saltRounds);
-  
-    await userCollection.insertOne({
-      username: username,
-      password: hashedPassword,
-      email: email,
-      phone: phone,
-    });
-    console.log("User has been inserted");
-  
-    req.session.authenticated = true;
-    req.session.username = username;
-    req.session.remainingQuantity = 10
-    res.redirect("/index");
+  var hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  await userCollection.insertOne({
+    username: username,
+    password: hashedPassword,
+    email: email,
+    phone: phone,
   });
+  console.log("User has been inserted");
+
+  req.session.authenticated = true;
+  req.session.username = username;
+  req.session.remainingQuantity = 10
+  res.redirect("/index");
+});
 
 
-  app.get("/redeem", (req, res) => {
-    // if (!req.session.authenticated) {
-    //   res.redirect("/");
-    //   return;
-    // }
-    res.render("redeem");
-  });
+app.get("/redeem", (req, res) => {
+  // if (!req.session.authenticated) {
+  //   res.redirect("/");
+  //   return;
+  // }
+  res.render("redeem");
+});
 
-  app.get("/setting", (req, res) => {
-    if (!req.session.authenticated) {
-      res.redirect("/");
-      return;
-    }
-    res.render("setting");
-  });
+app.get("/setting", (req, res) => {
+  if (!req.session.authenticated) {
+    res.redirect("/");
+    return;
+  }
+  res.render("setting");
+});
 
-  app.get("/warehouse", (req, res) => {
-    // if (!req.session.authenticated) {
-    //   res.redirect("/");
-    //   return;
-    // }
-    res.render("warehouse");
-  });
+app.get("/warehouse", (req, res) => {
+  // if (!req.session.authenticated) {
+  //   res.redirect("/");
+  //   return;
+  // }
+  res.render("warehouse");
+});
 
 app.get("/getRemainingQuantity", (req, res) => {
   if (!req.session.authenticated) {
@@ -188,70 +188,70 @@ app.post("/updateRemainingQuantity", (req, res) => {
   res.json({ remainingQuantity: req.session.remainingQuantity });
 });
 
-  app.get("/index", (req, res) => {
-    if (!req.session.authenticated) {
-      res.redirect("/");
-      return;
-    }
-    
-    res.render("index", { username: req.session.username });
+app.get("/index", (req, res) => {
+  if (!req.session.authenticated) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("index", { username: req.session.username });
+});
+
+app.get("/event", (req, res) => {
+  if (!req.session.authenticated) {
+    res.redirect("/");
+    return;
+  }
+  res.render("event");
+});
+
+app.get("/login", (req, res) => {
+  const errorMsg = req.query.errorMsg;
+  res.render("login", { errorMsg });
+});
+
+app.post("/loginSubmit", async (req, res) => {
+
+  var email = req.body.email;
+  var password = req.body.password;
+
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().max(20).required(),
   });
 
-  app.get("/event", (req, res) => {
-    if (!req.session.authenticated) {
-      res.redirect("/");
-      return;
-    }
-    res.render("event");
-  });
-  
-  app.get("/login", (req, res) => {
-    const errorMsg = req.query.errorMsg;
-    res.render("login", { errorMsg });
-  });
+  const validationResult = schema.validate({ email, password });
+  if (validationResult.error != null) {
+    console.log(validationResult.error);
+    res.redirect("/login");
+    return;
+  }
 
-  app.post("/loginSubmit", async (req, res) => {
+  const result = await userCollection.find({ email: email }).project({ email: 1, password: 1, _id: 1, username: 1 }).toArray();
 
-    var email = req.body.email;
-    var password = req.body.password;
-  
-    const schema = Joi.object({
-      email: Joi.string().email().required(),
-      password: Joi.string().max(20).required(),
-    });
-  
-    const validationResult = schema.validate({ email, password });
-    if (validationResult.error != null) {
-      console.log(validationResult.error);
-      res.redirect("/login");
-      return;
-    }
-  
-    const result = await userCollection.find({email: email}).project({ email: 1, password: 1, _id: 1, username: 1 }).toArray();
-  
-    console.log(result);
-    if (result.length != 1) {
-      console.log("User is not found...");
-      res.redirect("/login");
-      return;
-    }
-    if (await bcrypt.compare(password, result[0].password)) {
-      console.log("right password");
-  
-      req.session.authenticated = true;
-      req.session.username = result[0].username;
-      req.session.cookie.maxAge = expireTime;
-  
-      res.redirect("/index");
-      return;
-    } else {
-      console.log("wrong password");
-      res.redirect("/login?errorMsg=Invalid email/password combination.");
-      return;
-    }
-  });
+  console.log(result);
+  if (result.length != 1) {
+    console.log("User is not found...");
+    res.redirect("/login");
+    return;
+  }
+  if (await bcrypt.compare(password, result[0].password)) {
+    console.log("right password");
 
-  
+    req.session.authenticated = true;
+    req.session.username = result[0].username;
+    req.session.cookie.maxAge = expireTime;
+
+    res.redirect("/index");
+    return;
+  } else {
+    console.log("wrong password");
+    res.redirect("/login?errorMsg=Invalid email/password combination.");
+    return;
+  }
+});
+
+
 
 app.listen(port, () => {
   console.log("Node application listening on port " + port);
