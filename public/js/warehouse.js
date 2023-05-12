d7e1a28381c7d59049b115b2f01f8ecc876f7d57
EddieQVQ <122253032+EddieQVQ@users.noreply.github.com>
2023-05-12 13:47:18 -0700

    Update warehouse.js

--- public/js/warehouse.js
+++ public/js/warehouse.js
@@ -3,5 +3,5 @@ copyButton.addEventListener("click", () => {
   const cdKey = document.querySelector(".key1_details").textContent;
   navigator.clipboard.writeText(cdKey);
   alert("CD key copied to clipboard: " + cdKey);
-});
+}); 
 
