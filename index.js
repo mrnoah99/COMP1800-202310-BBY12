576596b438bb063f8110e7f0dbf0bbd110cd32fa
Noah <nbaldwin3@my.bcit.ca>
2023-05-18 22:13:30 -0700

    stuff

--- index.js
+++ index.js
@@ -56,28 +56,27 @@ app.use(session({
 
 // Gets the 55,000 games dataset loaded into a variable accessible by the rest of index.js
 const fs = require("fs");
-    fs.readFile("public/datasets/steam_games_test.json", 'UTF-8', (err, data) => {
-        if (err) {
-            console.error("Error reading file: ", err);
-            return;
-        }
+fs.readFile("public/datasets/steam_games_test.json", 'UTF-8', (err, data) => {
+  if (err) {
+    console.error("Error reading file: ", err);
+    return;
+  }
 
-        try {
-            gamesJSONData = JSON.parse(data);
-            console.log("All games:\n" + gamesJSONData);
-            console.log("\nFirst game:\n" + gamesJSONData[0]);
-            console.log("Game title: " + gamesJSONData[0].name);
-        } catch (error) {
-            console.error("Error parsing JSON: ", error);
-        }
-    });
+  try {
+    gamesJSONData = JSON.parse(data);
+    console.log("All games:\n" + gamesJSONData);
+  } catch (error) {
+    console.error("Error parsing JSON: ", error);
+  }
+});
 
 app.get("/", (req, res) => {
   if (!req.session.authenticated) {
     const errorMsg = req.query.errorMsg;
-    res.render("login", { errorMsg });
+    console.log(errorMsg)
+    res.redirect("/login");
   } else {
-    res.render("index", {title: "Home Page"});
+    res.render("index", { title: "Home Page" });
   }
 });
 
@@ -131,7 +130,7 @@ app.post("/loginSubmit", async (req, res) => {
   }
 });
 
-app.get('/nosql-injection', async (req,res) => {
+app.get('/nosql-injection', async (req, res) => {
   var name = req.query.user;
 
   if (!name) {
@@ -143,45 +142,45 @@ app.get('/nosql-injection', async (req,res) => {
   const schema = Joi.string().max(100).required();
   const validationResult = schema.validate(name);
 
-    var invalid = false;
+  var invalid = false;
   //If we didn't use Joi to validate and check for a valid URL parameter below
   // we could run our userCollection.find and it would be possible to attack.
   // A URL parameter of user[$ne]=name would get executed as a MongoDB command
   // and may result in revealing information about all users or a successful
   // login without knowing the correct password.
-  if (validationResult.error != null) { 
-        invalid = true;
-      console.log(validationResult.error);
-  //    res.send("<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>");
-  //    return;
-  }	
-    var numRows = -1;
-    //var numRows2 = -1;
-    try {
-      const result = await userCollection.find({name: name}).project({username: 1, password: 1, _id: 1}).toArray();
-      //const result2 = await userCollection.find("{name: "+name).project({username: 1, password: 1, _id: 1}).toArray(); //mongoDB already prevents using catenated strings like this
-        //console.log(result);
-        numRows = result.length;
-        //numRows2 = result2.length;
-    }
-    catch (err) {
-        console.log(err);
-        res.send(`<h1>Error querying db</h1>`);
-        return;
-    }
+  if (validationResult.error != null) {
+    invalid = true;
+    console.log(validationResult.error);
+    //    res.send("<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>");
+    //    return;
+  }
+  var numRows = -1;
+  //var numRows2 = -1;
+  try {
+    const result = await userCollection.find({ name: name }).project({ username: 1, password: 1, _id: 1 }).toArray();
+    //const result2 = await userCollection.find("{name: "+name).project({username: 1, password: 1, _id: 1}).toArray(); //mongoDB already prevents using catenated strings like this
+    //console.log(result);
+    numRows = result.length;
+    //numRows2 = result2.length;
+  }
+  catch (err) {
+    console.log(err);
+    res.send(`<h1>Error querying db</h1>`);
+    return;
+  }
 
-    console.log(`invalid: ${invalid} - numRows: ${numRows} - user: `,name);
+  console.log(`invalid: ${invalid} - numRows: ${numRows} - user: `, name);
 
-    // var query = {
-    //     $where: "this.name === '" + req.body.username + "'"
-    // }
+  // var query = {
+  //     $where: "this.name === '" + req.body.username + "'"
+  // }
 
-    // const result2 = await userCollection.find(query).toArray(); //$where queries are not allowed.
-    
-    // console.log(result2);
+  // const result2 = await userCollection.find(query).toArray(); //$where queries are not allowed.
 
-    res.send(`<h1>Hello</h1> <h3> num rows: ${numRows}</h3>`); 
-    //res.send(`<h1>Hello</h1>`);
+  // console.log(result2);
+
+  res.send(`<h1>Hello</h1> <h3> num rows: ${numRows}</h3>`);
+  //res.send(`<h1>Hello</h1>`);
 
 });
 
@@ -259,10 +258,10 @@ app.post("/updateRemainingQuantity", (req, res) => {
 
 app.get("/index", (req, res) => {
   if (!req.session.authenticated) {
-    res.redirect("/");
+    res.redirect("/login");
     return;
   }
-  
+
   res.render("index", { username: req.session.username, title: "Home Page" });
 });
 
@@ -271,7 +270,7 @@ app.get("/event", (req, res) => {
     res.redirect("/");
     return;
   }
-  res.render("event", {title: "Event"});
+  res.render("event", { title: "Event" });
 });
 
 app.get("/profile", (req, res) => {
@@ -279,7 +278,7 @@ app.get("/profile", (req, res) => {
     res.redirect("/");
     return;
   }
-  res.render("profile", {nickname: "test", email: "test@email.ca"});
+  res.render("profile", { nickname: "test", email: "test@email.ca" });
 });
 
 app.get("/pricecompare", (req, res) => {
@@ -295,7 +294,7 @@ app.get("/recommended", (req, res) => {
     res.redirect("/");
     return;
   }
-  res.render("recommend", {imageUrl1: "/img/steam_logo.png", imageUrl2: "/img/search.png"});
+  res.render("recommend", { imageUrl1: "/img/steam_logo.png", imageUrl2: "/img/search.png" });
 });
 
 app.get("/settings", (req, res) => {
@@ -303,7 +302,7 @@ app.get("/settings", (req, res) => {
     res.redirect("/");
     return;
   }
-  res.render("settings");
+  res.render("settings", { title: settings });
 });
 
 app.get("/notif-settings", (req, res) => {
@@ -322,26 +321,77 @@ app.get("/sec-settings", (req, res) => {
   res.render("sec-settings");
 });
 
+app.get('/gamedetails', (req, res) => {
+  let gameID = req.query.game_ID; // SteamID for the game, allows for the code here to get all of the other details for the game.
+  let resultIndex = 0;
+  if (!req.query.game_ID) {
+    gameID = 0;
+  }
+  for (let i = 0; i < gamesJSONData.length; i++) {
+    if (gamesJSONData[i].appid == gameID) {
+      resultIndex = i;
+    }
+  }
+  let game = gamesJSONData[resultIndex];
+  let gameName = game.name; // 실제 게임 이름으로 대체해야 합니다.
+  let gameRating = Math.round((game.positive / (game.positive + game.negative)) * 10000) / 100; // 실제 게임 평점으로 대체해야 합니다.
+  let gameDescription = game.short_description; // 실제 게임 설명으로 대체해야 합니다.
+  let gameImage = game.header_image; // 실제 게임 이미지 경로로 대체해야 합니다.
+  let similarGames = `<a href='/gamedetails?game_ID=${gamesJSONData[1].appid}'><img id='${gamesJSONData[1].appid}' class='moregame' onmouseleave='closeHoverMenu()' onmouseenter='openHoverMenu(${gamesJSONData[1].appid})' src='${gamesJSONData[1].header_image}'></a>`; // 실제 유사한 게임 목록으로 대체해야 합니다.
+
+  res.render('gamedetail', {
+    gameName: gameName, gameRating: gameRating, gameDescription: gameDescription,
+    gameImage: gameImage, similarGames: similarGames, title: `${gameName} Details`,
+    truncatedDesc: `${gamesJSONData[1].short_description.substring(0, 200)}...`,
+    moreGameName: `${gamesJSONData[1].name}`
+  });
+
+
+});
+
 app.get("/searchresults", (req, res) => {
   let search = req.query.search;
-  let capitalised = search.substring(0, 1).toUpperCase + search.substring(1, search.length);
-  let allcaps = search.toUpperCase;
+  let capitalised = search.substring(0, 1).toUpperCase() + search.substring(1, search.length);
+  let allcaps = search.toUpperCase();
   let result = -1;
+  let searchResult = "";
+  if ("Counter-Strike".includes("Counter")) {
+    console.log("Counter will return the game Counter Strike.");
+  } else {
+    console.log("Counter will not return the game Counter Strike");
+  }
+  if (search.length == 1) {
+    capitalised = search.toUpperCase();
+  }
   for (i = 0; i < gamesJSONData.length; i++) {
     if (gamesJSONData[i].name.includes(search) || gamesJSONData[i].name.includes(capitalised) || gamesJSONData[i].name.includes(allcaps)) {
       result = i;
+      let storeLink = gamesJSONData[result].name.replace(" ", "_");
+      let total = gamesJSONData[result].positive + gamesJSONData[result].negative;
+      console.log(`localhost:3200/gamedetails?game_name=${gamesJSONData[result].name}&ratings=${(gamesJSONData[result].positive / total) * 100}&imageurl=${gamesJSONData[result].header_image}&desc='${gamesJSONData[result].short_description.replace("'", "%27")}'`);
+      searchResult += `
+      <div class='card'>
+        <p class='game-title'>${gamesJSONData[result].name}</p>
+        <img class='game-img' src=${gamesJSONData[result].header_image}>
+        <span class='desc'>${gamesJSONData[result].short_description}</span>
+        <p class='overall-rating'>${Math.round(((gamesJSONData[result].positive / total) * 100) * 100) / 100}% Positive Reviews</p>
+        <div class='links'>
+          <a href='https://store.steampowered.com/app/${gamesJSONData[result].appid}/${storeLink}' target=_blank>Store Page</a>
+          <br>
+          <a class='btn btn-primary detail-button' href='/gamedetails?game_ID=${gamesJSONData[result].appid}'>More Details</a>
+        </div>
+      </div>
+      `;
     }
   }
   if (result <= -1) {
     result = 0;
   }
-  console.log("Result: " + gamesJSONData[result].name);
   if (!req.session.authenticated) {
     res.redirect("login");
     return;
   }
-  res.render("searchresults", {title: "Search Results", search_query: search, data: gamesJSONData[0].name, 
-  image: gamesJSONData[0].header_image});
+  res.render("searchresults", { title: "Search Results", search_query: search, search_results: searchResult });
 });
 
 // Will need to be connected to actual log out functions.
@@ -356,7 +406,7 @@ app.get("/logout", (req, res) => {
 app.get("/csvexample", (req, res) => {
   let input = "Test";
   let result = input;
-  res.render("csvexample", {search_query: result, title: "Test"});
+  res.render("csvexample", { search_query: result, title: "Test" });
 });
 
 app.listen(port, () => {
