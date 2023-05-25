9c28a7408d50b9224e6c657f639b5a0198957951
EddieQVQ <wxu55@my.bcit.ca>
2023-05-25 13:20:02 -0700

    merge

--- models/post.js
+++ models/post.js
@@ -1,5 +1,3 @@
-
-// post.js
 const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
@@ -12,31 +10,4 @@ const PostSchema = new mongoose.Schema({
 const Post = mongoose.model('Post', PostSchema);
 
 module.exports = Post;
-=======
-const express = require('express');
-const router = express.Router();
-const Post = require('../models/post');
-
-router.post('/', async (req, res) => {
-    try {
-        let obj;
-        
-        obj = {
-            writer: req.body.writer,
-            password: req.body.password,
-            title: req.body.title,
-            content: req.body.content
-        };
-
-        const post = new Post(obj);
-        await post.save();
-        res.json({message: "post uploaded"});
-
-    } catch (err) {
-        console.log(err);
-        res.json({message: false});
-    }
-});
-
-module.exports = router;
 
